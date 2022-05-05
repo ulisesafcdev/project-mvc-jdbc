e7ae71c20dd8a799d262a0548e76444937b246ce
ulisesafcdev <ulisesafc2020@gmail.com>
2022-05-04 23:56:36 -0600

    Clase main donde instanciamos las clases de las vistas para mostrar e insertar datos en la tabla

---
+++ project-mvc-jdbc/src/init/AppLibros.java
@@ -0,0 +1,18 @@
+package init;
+
+import vista.MostrarLibros;
+import vista.NuevoLibro;
+
+public class AppLibros {
+
+	public static void main(String[] args) throws Exception {
+		
+		MostrarLibros mostrar_libros = new MostrarLibros();
+		// mostrar_libros.readLibros();
+		
+		NuevoLibro nuevo_libro = new NuevoLibro();
+		// nuevo_libro.createLibro("Manejo del DOM", "odiseoafcdev", "undercode");
+
+	}
+
+}
