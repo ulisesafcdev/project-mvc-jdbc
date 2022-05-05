2e4857430e0e599f10752f86d7d27170fdc34016
ulisesafcdev <ulisesafc2020@gmail.com>
2022-05-04 23:57:06 -0600

    Clase main donde se hacen pruebas para comprobar la conexion a la bbdd

---
+++ project-mvc-jdbc/src/test/testConn.java
@@ -0,0 +1,13 @@
+package test;
+
+import controlador.ConexionBBDD;
+
+public class testConn {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		ConexionBBDD c = new ConexionBBDD();
+		c.Conectar();
+	}
+
+}
