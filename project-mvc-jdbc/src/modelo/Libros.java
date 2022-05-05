5d91153bd2620c7f76615cb157b425b8c71b62ad
ulisesafcdev <ulisesafc2020@gmail.com>
2022-05-04 23:53:13 -0600

    Modelo de la tabla en la base de datos

---
+++ project-mvc-jdbc/src/modelo/Libros.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Libros {
+	
+	private String titulo;
+	
+	private String author;
+	
+	private String editorial;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+	
+	
+
+}
